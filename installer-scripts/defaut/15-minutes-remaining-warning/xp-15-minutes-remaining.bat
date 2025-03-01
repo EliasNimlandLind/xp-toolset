566fe32017f61ddb1ee4dbcfc84e9e5e45ffe612
EliasNimlandLind <elias.nimland.lind1@gmail.com>
2025-03-01 01:35:19 +0100

    Add time remaining warning

---
+++ installer-scripts/defaut/15-minutes-remaining-warning/xp-15-minutes-remaining.bat
@@ -0,0 +1,10 @@
+@echo off
+set "file=C:\Users\elias\source\repos\15-minutes-remaining-warning\audio-warning.mp3"
+( echo Set Sound = CreateObject("WMPlayer.OCX.7"^)
+  echo Sound.URL = "%file%"
+  echo Sound.Controls.play
+  echo do while Sound.currentmedia.duration = 0
+  echo wscript.sleep 100
+  echo loop
+  echo wscript.sleep (int(Sound.currentmedia.duration^)+1^)*1000) >sound.vbs
+start /min sound.vbs
